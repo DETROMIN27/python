593394da13085846ee69e2e4c04fe2f86846d9cf
DETROMIN27 <polakdasa1@gmail.com>
2023-10-28 21:46:56 +0300

    rhrerrrrrrrrrrrrrrrrrrrrrrrr

---
+++ window.test/main.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QApplication
+app = QApplication([])
+from main_win import*
+
+
+
+test_win.show()
+app.exec()
---
+++ window.test/main_win.py
@@ -0,0 +1,91 @@
+from PyQt5.QtWidgets import (QWidget,QLabel,QHBoxLayout,QVBoxLayout,
+                             QPushButton,QRadioButton,QSpinBox,QGroupBox,QButtonGroup,
+                             )
+from PyQt5.QtCore import Qt
+
+test_win = QWidget()
+test_win.resize(600,500)
+test_win.setWindowTitle("Memory Card")
+
+
+btn_menu = QPushButton("Menu")
+btn_rest = QPushButton("Time")
+time_rest = QSpinBox()
+time_rest.setValue(30)
+lbl_rest = QLabel("minutes")
+
+lbl_qw = QLabel("Question")
+
+box_ans = QGroupBox()
+box_ans.setTitle("Answers")
+ 
+rbn_list = list()
+rbn_group = QButtonGroup()
+for i in range (4):
+    rbt = QRadioButton("a")
+    rbn_group.addButton(rbt)
+    rbn_list.append(rbt)
+
+box_result = QGroupBox()
+box_result.setTitle("Result")
+lbl_ans = QLabel("ans")
+lbl_result = QLabel("result")
+
+btn_check = QPushButton("Check")
+
+main_line = QVBoxLayout()
+lineh1 = QHBoxLayout()
+lineh2 = QHBoxLayout()
+lineh3 = QHBoxLayout()
+lineh4 = QHBoxLayout()
+
+
+lineh1.addWidget(btn_menu)
+lineh1.addStretch(3)
+lineh1.addWidget(btn_rest)
+lineh1.addWidget(time_rest)
+lineh1.addWidget(lbl_rest)
+
+lineh2.addWidget(lbl_qw,alignment=Qt.AlignCenter)
+lineh3.addWidget(box_result)
+box_result.hide()
+lineh3.addWidget(box_ans)
+
+lineh4.addStretch(2)
+lineh4.addWidget(btn_check)
+lineh4.addStretch(2)
+
+line_v1 = QVBoxLayout()
+line_v1.addWidget(lbl_result,alignment=Qt.AlignLeft)
+line_v1.addWidget(lbl_ans, alignment=Qt.AlignCenter)
+box_result.setLayout(line_v1)
+
+line_v2 = QVBoxLayout()
+line_v3 = QVBoxLayout()
+lineh5 = QHBoxLayout()
+
+line_v2.addWidget(rbn_list[0])
+line_v2.addWidget(rbn_list[1])
+line_v3.addWidget(rbn_list[2])
+line_v3.addWidget(rbn_list[3])
+lineh5.addLayout(line_v2)
+lineh5.addLayout(line_v3)
+box_ans.setLayout(lineh5)
+
+
+
+
+main_line.addLayout(lineh1)
+main_line.addLayout(lineh2)
+main_line.addLayout(lineh3)
+main_line.addLayout(lineh4)
+
+test_win.setLayout(main_line)
+
+
+
+
+
+
+
+
