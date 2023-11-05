b3481b9c1dd1aafd2fba3c9d31b4046a3557e5e9
DETROMIN27 <polakdasa1@gmail.com>
2023-11-05 13:32:46 +0200

    іефке

--- window.test/edit_win.py
+++ window.test/edit_win.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import (QWidget,QLabel,QPushButton,QLineEdit,QGridLayout)
  
-edit_win = QLabel()
+edit_win = QWidget()
+edit_win.resize(600,500)
+edit_win.move(300,300)
 
 lbl_qw_edit = QLabel("Qw qw")
 lbl_ans_edit = QLabel("Right answ text")
@@ -27,3 +29,30 @@ btn_back = QPushButton("back")
 grid = QGridLayout()
 grid.addWidget(lbl_qw_edit,0,0)
 grid.addWidget(edit_qw,0,1)
+
+grid.addWidget(lbl_ans_edit,1,0)
+grid.addWidget(edit_ans,1,1)
+
+grid.addWidget(lbl_wr1_edit,2,0)
+grid.addWidget(edit_wrong1,2,1)
+
+grid.addWidget(lbl_awr2_edit,3,0)
+grid.addWidget(edit_wrong2,3,1)
+
+grid.addWidget(lbl_wr3_edit,4,0)
+grid.addWidget(edit_wrong3,4,1)
+
+grid.addWidget(btn_add,5,0)
+grid.addWidget(btn_clear,5,1)
+
+grid.addWidget(lbl_stat,6,0,2,0)
+grid.addWidget(lbl_count,7,0,2,0)
+grid.addWidget(lbl_count_right,8,0,2,0)
+grid.addWidget(lbl_good,9,0,2,0)
+
+grid.addWidget(btn_back,10,0,2,0)
+
+edit_win.setLayout(grid)
+edit_win.hide()
+
+
--- window.test/main.py
+++ window.test/main.py
@@ -1,12 +1,28 @@
 from PyQt5.QtWidgets import QApplication,QMessageBox
 app = QApplication([])
 from main_win import*
-from data import qw_list
+from data import qw_list,Question
 from random import shuffle, choice
+from edit_win import*
 
 choice_ans = ""
 qw = ""
 
+def stat():
+    count = 0
+    count_r = 0 
+    good = 0 
+    
+    for q in qw_list:
+        count += q.count 
+        count_r += q.count_right
+    if count != 0 :
+        good = count_r/count*100 
+    else:
+        good = 0
+    lbl_count.setText(f"Count of answers: {count}")
+    lbl_count_right.setText(f"Count of right answers: {count_r}")
+    lbl_good.setText(f"Count of good: {good}")
 
 def new_qw ():
     global qw
@@ -59,8 +75,44 @@ def click_ans(rbn):
 
 
 new_qw()
+def show_menu():
+    stat()
+    test_win.hide()
+    edit_win.show()
+
+
+def show_test():
+    test_win.show()
+    edit_win.hide()
+def clear ():
+    edit_ans.clear()
+    edit_qw.clear()
+    edit_wrong1.clear()
+    edit_wrong2.clear() 
+    edit_wrong3.clear()
+
+def add_qw():
+    qw_text = edit_qw.text()
+    ans_text = edit_ans.text()
+    wrg1_tetx = edit_wrong1.tetx()
+    wrg2_tetx = edit_wrong2.tetx()
+    wrg3_tetx = edit_wrong3.tetx() 
+    if qw_text != "" and ans_text != "" and wrg1_tetx != "" and wrg2_tetx != "" and wrg3_tetx !="":
+        q =  Question(qw_text,ans_text,wrg1_tetx,wrg2_tetx,wrg3_tetx)
+        qw_list.append(q)
+        clear()
+    else:
+        mess = QMessageBox()
+        mess.setText("text")
+        mess.show()
+        mess.exec()
+
 rbn_group.buttonClicked.connect(click_ans)
 btn_check.clicked.connect(check_ans)
+btn_menu.clicked.connect(show_menu)
+btn_add.clicked.connect(add_qw)
+btn_back.clicked.connect(show_test)
+btn_clear.clicked.connect(clear)
 
 test_win.show()
 app.exec()
